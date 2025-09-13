171e3f3448a62304b6b4873ed8bfa3d64c054c4b
Husni Kusuma <husnikusuma00@gmail.com>
2025-09-13 22:11:32 +0700

    Remove .docusaurus from repo and update .gitignore

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/husni/PROJECT-ENVOYOU-API/app-envoyou/node_modules/infima/dist/css/default/default.css"),
-  require("/home/husni/PROJECT-ENVOYOU-API/app-envoyou/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/husni/PROJECT-ENVOYOU-API/app-envoyou/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/husni/PROJECT-ENVOYOU-API/app-envoyou/docusaurus-theme/css/custom.css"),
-];
